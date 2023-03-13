145d93bee14b3736e473063cfc5984396811cf4b
BrynjolfurS <brynjolfurstein@gmail.com>
2023-03-13 13:24:32 +0000

    LibrarySystem skeleton

--- src/is/hi/hbv202g/ass8/services/LibrarySystem.java
+++ src/is/hi/hbv202g/ass8/services/LibrarySystem.java
@@ -1,9 +1,8 @@
 package is.hi.hbv202g.ass8.services;
 
-import is.hi.hbv202g.ass8.entities.Author;
-import is.hi.hbv202g.ass8.entities.Book;
-import is.hi.hbv202g.ass8.entities.User;
+import is.hi.hbv202g.ass8.entities.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class LibrarySystem {
@@ -16,6 +15,36 @@ public class LibrarySystem {
     }
 
     public void addBookWithTitleAndAuthorlist(String title, List<Author> authors) {
+        Book bookToAdd = new Book(title, authors);
+        books.add(bookToAdd);
+    }
+
+    public void addStudentUser(String name, boolean feePaid) {
+        Student newStudentUser = new Student(name, feePaid);
+        users.add(newStudentUser);
+    }
+
+    public void addFacultyMemberUser(String name, String department) {
+        FacultyMember newFacultyMemberUser = new FacultyMember(name, department);
+    }
+
+    public Book findBookByTitle(String title) {
+
+    }
+
+    public User findUserByName(String name) {
+
+    }
+
+    public void borrowBook(User user, Book book) {
+
+    }
+
+    public void extendLending(FacultyMember facultyMember, Book book, LocalDate newDueDate) {
+
+    }
+
+    public void returnBook(User user, Book book) {
 
     }
 }
