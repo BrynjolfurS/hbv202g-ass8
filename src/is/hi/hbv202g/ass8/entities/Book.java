7cc28d31234f7422ab9fc877f353fcb4ecac7a2c
BrynjolfurS <brynjolfurstein@gmail.com>
2023-03-13 12:53:35 +0000

    Pakkastrúktur og klasar

---
+++ src/is/hi/hbv202g/ass8/entities/Book.java
@@ -0,0 +1,12 @@
+package is.hi.hbv202g.ass8.entities;
+
+/***********************************************************
+ * Nafn: Brynjólfur Steingrímsson
+ * Email: brs26@hi.is
+ *
+ * Lýsing:
+ *
+ *
+ ***********************************************************/
+public class Book {
+}
